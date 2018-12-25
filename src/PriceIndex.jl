6e1306f3fc657e6f1e18007a1e154e4299cb6b33
sejinahn2 <43711842+sejinahn2@users.noreply.github.com>
2018-12-25 09:40:12 -0800

    Update PriceIndex.jl

--- src/PriceIndex.jl
+++ src/PriceIndex.jl
@@ -2,4 +2,18 @@ module PriceIndex
 
 greet() = print("Hello World!")
 
+function FixedLaspeyres(p,q)
+N=size(p,1)
+T=size(p,2)
+Pl=zeros(N+1,T)
+s=zeros(N,T)
+   for t in 1:T
+        for n in 2:N+1
+        s[n-1,t] = (p[n-1,t]*q[n-1,t])/dot(p[:,t],q[:,t])
+        Pl[n,t]=Pl[n-1,t]+p[n-1,t]/p[n-1,1]*s[n-1,1]
+        end
+    end
+return Pl[N+1,:]
+end
+
 end # module
