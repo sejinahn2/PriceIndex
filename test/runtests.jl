2e2e1116668c66febdb1a0fb6e0865ae377e99e7
sejinahn2 <43711842+sejinahn2@users.noreply.github.com>
2018-12-25 10:10:43 -0800

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,9 @@ using PriceIndex
 using Test
 
 @testset "PriceIndex.jl" begin
-    # Write your own tests here.
+
+@test foo() == 0.11388071406436832
+@test foo(1, 1.5) == 0.2731856314283442
+@test_broken foo(1, 0) # tells us this is broken Write your own tests here.
+
 end
